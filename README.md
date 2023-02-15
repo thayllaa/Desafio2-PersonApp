135d64ef26046d83e082d881c4fef6765e94c6b7
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-02-15 20:45:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafio2-PersonApp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zzhrbq)
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>{{name}}</h1>
+  `,
+})
+export class App {
+  name = 'Person App';
+}
+
+bootstrapApplication(App);
